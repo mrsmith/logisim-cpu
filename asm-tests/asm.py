fc675e14483c8cd54c1b833250d4f84bb8bfad6b
Alexey Kuznetsov <kuznecov.alexey@gmail.com>
2019-11-14 22:37:23 -0800

    add imma/immb

--- asm-tests/asm.py
+++ asm-tests/asm.py
@@ -27,9 +27,10 @@ def insn(foo):
     return foo
 
 CTL_JMP = 1 << 0
-CTL_MWR = 1 << 1
+# CTL_MWR = 1 << 1
 CTL_WRA = 1 << 2
 CTL_WRB = 1 << 3
+CTL_LDMEM_LDIMM = 1 << 4
 
 def encode(imm, ctl):
     return '{:02x}{:02x}'.format(imm, ctl)
@@ -87,6 +88,34 @@ def lda(ln, line, out):
 @insn
 def ldb(ln, line, out):
     return ldr('b', line, out)
+
+def immr(r, line, out):
+    ''' template for imma, immb instructions '''
+    assert r in {'a', 'b'}
+    insn = 'imm' + r
+
+    try:
+        _, imm = line.split(' ', 1)
+        imm = int(imm)
+
+    except ValueError:
+        raise AsmError('bad {} instruction `{}`'.format(insn, line))
+
+    if imm < -128 or imm > 255:
+        raise AsmError('bad {} imm `{}`'.format(insn, imm))
+
+    imm = dbl_comp_8(imm)
+
+    ctl = dict(a=CTL_WRA, b=CTL_WRB)
+    out.writeln(encode(imm=imm, ctl=CTL_LDMEM_LDIMM | ctl[r]))
+
+@insn
+def imma(ln, line, out):
+    return immr('a', line, out)
+
+@insn
+def immb(ln, line, out):
+    return immr('b', line, out)
 
 def main():
     args = parse_args()
