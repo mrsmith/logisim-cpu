69e366e8c6b8a755f693d65c0e27ec408c81549d
Alexey Kuznetsov <kuznecov.alexey@gmail.com>
2019-11-15 12:34:23 -0800

    cleanup fib2

--- asm-tests/fib2.asm
+++ asm-tests/fib2.asm
@@ -4,8 +4,6 @@
 # a, b = b, a + b
 #
 
-# cycle counter, do for 3 iterations
-
 imma 10
 sta 2   # @2: count
 
@@ -17,23 +15,16 @@ sta 1
 
 # loop
 lda 0
-nop
 ldb 1
-nop
 add
 sta 1   # b = a + b
 stb 0   # a = b
-nop
-nop
-nop
-nop
-nop
 lda 2
 immb 1
 sub     # count -= 1
 sta 2
 jz 2    # break
-jmp -17 # goto loop
+jmp -10 # goto loop
 
 # end
 hlt
